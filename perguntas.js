70ae8e7b36a4b4c9967260cf878434fd8068baf0
pesssoacomdepressao <146104216+pesssoacomdepressao@users.noreply.github.com>
2024-10-07 08:23:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Em que ano a Segunda Guerra Mundial começou?',
+    'No ano de 1939'
+)
+
+criaCartao(
+    'Português',
+    'Quem escreveu Dom Casmurro?',
+    'Foi o Machado de Assis'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'Paris'
+)
+
+criaCartao(
+    'Quimica',
+    'Atualemte, quantos elementos químicos a tabela peroódica possui?',
+    '118 Elementos'
+)
